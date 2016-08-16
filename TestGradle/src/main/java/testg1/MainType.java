a1c36454e213a9a83da9755c2195ae463b3c8ebf
Sergio <sergiolesnik@gmail.com>
2016-08-16 17:53:36 +0300

    added Simple gradle project

---
+++ TestGradle/src/main/java/testg1/MainType.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testg1;
+
+import com.google.gson.Gson;
+
+
+/**
+ *
+ * @author Less
+ */
+public class MainType {
+	
+	public static void main(String[] args){
+		System.out.println("Hello gradle!");
+		Gson gson = new Gson();
+	}
+	
+}
