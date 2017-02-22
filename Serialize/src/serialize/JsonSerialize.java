f89d9b3a05f685e0df002b6736218a7bfea7fdea
lesnikyan <sergiolesnik@gmail.com>
2017-02-23 00:04:19 +0200

    JSON serialization completed by list of objects.

--- Serialize/src/serialize/JsonSerialize.java
+++ Serialize/src/serialize/JsonSerialize.java
@@ -9,6 +9,10 @@
 package serialize;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -17,10 +21,14 @@ import com.google.gson.Gson;
 public class JsonSerialize {
 	
 	Gson gson = new Gson();
+        
+        String serializedData = "";
 	
 	public void test(){
 		serialize();
 		unserialize();
+                serializeComplex();
+                unserializeComplex();
 	}
 	
 	public void serialize(){
@@ -33,6 +41,29 @@ public class JsonSerialize {
 		String data = "{\"name\":\"Guns and Roses in desert\", \"author\":\"Sting\", \"year\":2010}";
 		Book book = gson.fromJson(data, Book.class);
 		p(book.getInfo());
+	}
+	
+	public void serializeComplex(){
+		Book book = new Book("Girls, girls, girls", "Kasanova Piterskiy", 2000);
+		String data = gson.toJson(book);
+		p(data);
+                List<LittleObject> objects = new ArrayList<>();
+                Type type = (new TypeToken<List<LittleObject>>(){}).getType();
+                for(int i=0; i< 5; ++i){
+                    objects.add(new LittleObject("little_" + i, i, i%2 == 0));
+                }
+                String dataList = gson.toJson(objects, type);
+                p(dataList);
+                serializedData = dataList;
+	}
+	
+	public void unserializeComplex(){
+		String data = "{\"name\":\"Guns and Roses in desert\", \"author\":\"Sting\", \"year\":2010}";
+		Book book = gson.fromJson(data, Book.class);
+		p(book.getInfo());
+                Type type = (new TypeToken<List<LittleObject>>(){}).getType();
+                List<LittleObject> list = gson.fromJson(serializedData, type);
+                p(list.get(0).name);
 	}
 	
 	public static void p(Object x){
@@ -54,4 +85,20 @@ class Book {
 	public String getInfo(){
 		return author + ". " + name +  ". " + year + ".";
 	}
+}
+
+class LittleObject {
+    String name;
+    int num;
+    boolean status;
+    
+    public LittleObject(){
+        
+    }
+    
+    public LittleObject(String name, int num, boolean status){
+        this.name = name;
+        this.num = num;
+        this.status = status;
+    }
 }
