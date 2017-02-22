9bce272ec54599f49763b278b02bd97e118da0a0
lesnikyan <sergiolesnik@gmail.com>
2017-02-22 17:04:57 +0200

    reinit

--- RegExp/src/regexp/RegExp.java
+++ RegExp/src/regexp/RegExp.java
@@ -3,13 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-package oop1;
+package regexp;
 
 /**
  *
- * @author less
+ * @author Less
  */
-public class Damage {
-    public int value;
+public class RegExp {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
 }
